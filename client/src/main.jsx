e3e287affa33c8e42432027c0b2adfb5700c8315
dipto230 <diptoroy230305@gmail.com>
2026-03-31 23:57:27 +0530

    added hero section

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,22 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import { ClerkProvider } from '@clerk/react'
+import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY= import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-if(!PUBLISHABLE_KEY){
-    throw new Error('Missing Publishable Key')
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
 }
-createRoot(document.getElementById('root')).render(
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    </ClerkProvider>
 
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </React.StrictMode>
 )
